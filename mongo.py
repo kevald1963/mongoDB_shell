b0596a7bb6e9ab929f328ee7bb944c525b6315fc
kevald1963 <kevin_alderson@yahoo.co.uk>
2019-12-03 00:06:48 +0000

    Examples of CRUD operations on a document database added to python file.

--- mongo.py
+++ mongo.py
@@ -3,7 +3,7 @@ import os
 
 MONGODB_URI = os.getenv("MONGO_URI")
 DBS_NAME = "myTestDB"
-COLLECTION_NAME = "myFirstDB"
+COLLECTION_NAME = "myFirstMDB"
 
 def mongo_connect(url):
     try:
@@ -19,7 +19,40 @@ conn = mongo_connect(MONGODB_URI)
 # Set our collection name on this database.
 coll = conn[DBS_NAME][COLLECTION_NAME]
 
+# Insert a single document (record) into the database.
+#Code below works fine. Just commented-out to avoid re-inserting more than once.
+#new_doc = {'first': 'douglas', 'last': 'adams', 'dob':'11/03/1952', 'hair_colour': 'grey', 'occupation':'writer', 'nationality': 'english'}
+#coll.insert(new_doc)
+
+# Insert multiple documents into the database.
+#new_docs = [ {'first': 'terry', 'last': 'pratchett', 'gender': 'm','dob':'28/04/1948', 'hair_colour': 'not much left!', 'occupation':'writer', 'nationality': 'english'}, {'first': 'george', 'last': 'rr martin', 'gender': 'm','dob':'20/09/1948', 'hair_colour': 'white', 'occupation':'writer', 'nationality': 'american'}]
+#coll.insert_many(new_docs)
+
 # Print out all the documents in our database.
-documents = coll.find()
+#documents = coll.find()
+#for doc in documents:
+#    print(doc)
+
+# Print out specific records e.g. where first name is 'douglas'.
+#documents = coll.find({'first': 'douglas'})
+#for doc in documents:
+#    print(doc)
+
+# Delete a specific record.
+#coll.remove({'first': 'douglas'})
+#documents = coll.find()
+#for doc in documents:
+#    print(doc)
+
+# Update one record, e.g. set hair colour to 'maroon' on first record where nationality is 'american'.
+#coll.update_one({'nationality': 'american'}, {'$set': {'hair_colour': 'maroon'}})
+#documents = coll.find({'nationality': 'american'})
+#for doc in documents:
+#    print(doc)
+
+# Update many record, e.g. set hair colour to 'maroon' on first record where nationality is 'american'.
+coll.update_many({'nationality': 'american'}, {'$set': {'hair_colour': 'maroon'}})
+documents = coll.find({'nationality': 'american'})
 for doc in documents:
     print(doc)
+
